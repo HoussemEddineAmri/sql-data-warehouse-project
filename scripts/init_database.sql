d4efac8d6324ab208a8b8a89452fc3429a3f84e0
Houssem Eddine Amri <amrieddinehoussem@gmail.com>
2025-06-27 17:35:01 +0100

    Add init_database script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,29 @@
 
+/* 
+Create Database and Schemas
+ */
+
+USE MASTER;
+GO
+
+-- Drop and recreate the 'DtaWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE NAME 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
